cdbafc8827cf0e3ca9dd8c59444cc7bb738b4e91
lhc220 <lhcode220@gmail.com>
2022-06-22 10:40:48 +0700

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const webpack = require('webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  mode: 'development',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.html$/,
+        use: 'html-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: '/public/index.html',
+    }),
+  ],
+};
